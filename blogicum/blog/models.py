b34e023f959f15613d57d1779177df10a142f01a
kapt0xa <kuzmich.ml@gmail.com>
2025-02-01 20:05:41 +0300

    core moved

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -5,27 +5,48 @@ from core.models import PublishedModel
 
 
 class Category(PublishedModel):
-    title = models.CharField('Название', max_length=256)
-    description = models.TextField()
-    slug = models.SlugField(unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField('Заголовок', max_length=256)
+    description = models.TextField('Описание')
+    slug = models.SlugField('Идетификатор', unique=True)
+    created_at = models.DateTimeField('Добавлено', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'категории'
+
+    def __str__(self):
+        return self.title
 
 
 class Location(PublishedModel):
-    name = models.CharField(max_length=256)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField('Название места', max_length=256)
+    created_at = models.DateTimeField('Добавлено', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Местоположение'
+        verbose_name_plural = 'местоположения'
+
+    def __str__(self):
+        return self.name
 
 
 class Post(PublishedModel):
-    title = models.CharField(max_length=256)
-    text = models.TextField()
-    pub_date = models.DateTimeField(blank=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               null=True)
-    location = models.ForeignKey(Location, on_delete=models.SET_NULL,
-                                 null=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL,
-                                 null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField('Заголовок', max_length=256)
+    text = models.TextField('Текст')
+    pub_date = models.DateTimeField('Дата и время публикации', blank=False)
+    author = models.ForeignKey('Автор публикации', User,
+                               on_delete=models.CASCADE, null=True)
+    location = models.ForeignKey('Местоположение', Location, 
+                                 on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey('Категория', Category,
+                                 on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField('Добавлено', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Публикация'
+        verbose_name_plural = 'публикации'
+
+    def __str__(self):
+        return self.title
 
 # Create your models here.

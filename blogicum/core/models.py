5239f161537e7f9ca0b3d64cdd1f5d2e8d78ac65
kapt0xa <kuzmich.ml@gmail.com>
2025-02-03 12:04:55 +0300

    pep8 in process

--- blogicum/core/models.py
+++ blogicum/core/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class PublishedModel(models.Model):
     """Abstract model. Adds flag is_published."""
 
-    is_published = models.BooleanField('Опубликовано', default=True, help_text='Снимите галочку, чтобы скрыть публикацию.')
+    is_published = models.BooleanField('Опубликовано',
+                                       default=True,
+                                       help_text='Снимите галочку, '
+                                       'чтобы скрыть публикацию.')
 
     class Meta:
         abstract = True

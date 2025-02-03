1b167026f4fa92851084aaa9668c975af34f5d1d
kapt0xa <kuzmich.ml@gmail.com>
2025-02-03 07:11:19 +0300

    no posts

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from core.models import PublishedModel 
+from core.models import PublishedModel
 
 
 class Category(PublishedModel):
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,38 +1,26 @@
 from django.shortcuts import render
 from django.http import Http404
 
-
-class PostStorage:
-    def __init__(self, posts_val=None, default=False):
-        if default:
-            posts_val = {}
-        self.posts = dict()
-        if posts_val:
-            for post in posts_val:
-                self.posts[post["id"]] = post
-
-
-post_storage = PostStorage(default=True)
+from .models import Post
 
 
 def index(request):
     return render(request, "blog/index.html",
-                  {"posts": reversed(post_storage.posts.items())})
+                  {"posts": Post.objects.filter(is_published=True)
+                   .order_by('pub_date')[:5]})
 
 
 def post_detail(request, post_id: int):
-    if post_id not in post_storage.posts:
-        raise Http404(f"post with id {id} does not exist")
+    try:
+        post = Post.objects.get(pk=post_id, is_published=True)
+    except Post.DoesNotExist:
+        raise Http404(f"Post with id {post_id} does not exist")
     return render(request, "blog/detail.html",
-                  {"post": post_storage.posts[post_id], "post_id": post_id})
+                  {"post": post, "post_id": post_id})
 
 
 def category_posts(request, category):
-    filtred = []
-    for id, post in post_storage.posts.items():
-        if post["category"] == category:
-            filtred.append((id, post))
-    return render(
-        request, "blog/category.html",
-        {"posts": reversed(filtred), "category": category}
-    )
+    return render(request, "blog/index.html",
+                  {"posts": Post.objects.filter(is_published=True,
+                                                category=category)
+                   .order_by('pub_date')[:5]})

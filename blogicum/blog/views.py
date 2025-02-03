5023b75a2b575b241f495fd81d324ff820798ab3
kapt0xa <kuzmich.ml@gmail.com>
2025-02-03 11:14:59 +0300

    pytest 12 failed

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -7,21 +7,20 @@ from .models import Post, Category
 
 def index(request):
     return render(request, "blog/index.html",
-                  {"post_list": Post.objects.filter(is_published=True).order_by('pub_date')[:5]})
+                  {"post_list": Post.objects.filter(is_published=True, pub_date__lte=timezone.now()).order_by('pub_date')[:5]})
 #pub_date__lte=timezone.now()
 
 
 def post_detail(request, post_id: int):
-    post = get_object_or_404(Post, pk=post_id, is_published=True)
+    post = get_object_or_404(Post, pk=post_id, is_published=True, pub_date__lte=timezone.now())
     return render(request, "blog/detail.html",
                   {"post": post, "post_id": post_id})
 
 
 def category_posts(request, category_slug):
-    Category.objects.filter(slug=category_slug, is_published=True)
     category = get_object_or_404(Category,
                                  slug=category_slug, is_published=True)
     return render(request, "blog/index.html",
-                  {"post_list": Post.objects.filter(is_published=True,
-                                                    category=category)
+                  {"post_list": Post.objects
+                   .filter(is_published=True,category=category, pub_date__lte=timezone.now())
                    .order_by('pub_date')[:5]})

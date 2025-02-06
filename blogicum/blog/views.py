63ce3b18e391e6489faa6501fef7881beee5786d
kapt0xa <kuzmich.ml@gmail.com>
2025-02-06 19:59:46 +0300

    slight changes

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import Http404
 
 from .models import Post, Category
 from .constants import MAX_POSTS_ON_PAGE
@@ -13,17 +12,13 @@ def index(request):
 
 
 def post_detail(request, post_id: int):
-    post = Post.get_by_id_or_404(post_id)
 
     return render(request, 'blog/detail.html',
-                  {'post': post})
+                  {'post': Post.get_by_id_or_404(post_id)})
 
 
 def category_posts(request, category_slug):
     category = Category.get_by_slug_or_404(category_slug)
-
-    if not category:
-        raise Http404(f'category {category_slug} not found')
 
     return render(request, 'blog/index.html',
                   {'post_list': Post
